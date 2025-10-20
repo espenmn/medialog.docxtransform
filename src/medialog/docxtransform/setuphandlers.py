67d6a32f4374e745adc78daf8409b48d0b5b985e
Espen Moe-Nilssen <espen@medialog.no>
2025-10-20 11:24:27 +0200

    first basic version

--- src/medialog/docxtransform/setuphandlers.py
+++ src/medialog/docxtransform/setuphandlers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+from Products.CMFCore.utils import getToolByName
+from plone import api
 
 
 @implementer(INonInstallable)
@@ -19,9 +21,44 @@ class HiddenProfiles(object):
 
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
-
+    # Registering our transform at the end of the installation of this package.
+    site =  api.portal.get() 
+    transforms_tool = getToolByName(site, 'portal_transforms')
+    
+    # Adding our file types to MTR
+    mtr = getToolByName(site, 'mimetypes_registry')
+    
+           
+    # if not mtr.lookup('application/vnd.openxmlformats-officedocument.wordprocessingml.document'):
+    #         mtr.manage_addMimeType(
+    #             id = "Office Word 2007 XML document",
+    #             mimetypes = ['application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
+    #             extensions = None,
+    #             icon_path = "word.png",
+    #             binary=1,
+    #         )
+    
+    if 'docx_to_text' not in transforms_tool.objectIds():
+        transforms_tool.manage_addTransform('docx_to_text', 'medialog.docxtransform.transform.doc_to_text') 
+        
+        
 
 def uninstall(context):
     """Uninstall script"""
     # Do something at the end of the uninstallation of this package.
+    # Is there any reason to uninstall mimetypes?
+    site =  api.portal.get() 
+    transforms_tool = getToolByName(site, 'portal_transforms')    
+    if 'docx_to_text' in transforms_tool.objectIds():        
+         transforms_tool.unregisterTransform('docx_to_text')
+         
+    # Removing our types from MTR
+    # mtr = getToolByName(site, 'mimetypes_registry')
+    # mtr.manage_delObjects('application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    
+
+
+
+
+
+  
---
+++ src/medialog/docxtransform/transform.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Our transform requires installation of docx2txt 
+"""
+import mimetypes
+from Products.PortalTransforms.interfaces import ITransform
+from zope.interface import implementer
+from Products.PortalTransforms.libtransforms.commandtransform import popentransform
+
+# Alternative 'manual transform:
+# from docx import Document 
+
+
+@implementer(ITransform)
+class docx_to_text(popentransform):
+    __name__ = "docx_to_text"
+    inputs = ["application/vnd.openxmlformats-officedocument.wordprocessingml.document"]
+    output = "text/plain"
+    output_encoding = "utf-8"    
+    binaryName = "docx2txt"
+    binaryArgs = "- -enc UTF-8 -"
+
+def register():
+    return docx_to_text()
